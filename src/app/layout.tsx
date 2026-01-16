7d146b5c9c8c58de9f2e38716dc50b9aed95fb89
User <user@example.com>
2026-01-16 22:02:36 +0300

    Replace favicon with company logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,7 +53,6 @@ export default function RootLayout({
   return (
     <html lang="tr">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
